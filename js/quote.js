82a5e5c6d46709db43f43b9c6bd3b5f44d324896
Timofei Shchepkin <dgrz5.0@gmail.com>
2019-06-26 12:46:14 -0400

    quote of the moment release! implemented with API and pure js

---
+++ js/quote.js
@@ -0,0 +1,54 @@
+fetch('https://graphql-api-backend.herokuapp.com/api', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+    },
+    body: JSON.stringify({ query: `
+                query {
+                    randomQuote {
+                        id
+                        text
+                        author
+                    }
+                }
+            `})
+})
+    .then(res => res.json())
+    .then(({data}) => data.randomQuote[0])
+    .then(q => document.getElementById('quote').innerHTML = q.text + ' - ' + '<em>' + q.author + '</em>');
+
+/*const { createApolloFetch } = require('./appolo-fetch.min');
+
+const fetch = createApolloFetch({
+    uri: 'https://graphql-api-backend.herokuapp.com/api',
+});
+
+fetch({
+    query: '{ posts { title }}',
+}).then(res => {
+    console.log(res.data);
+});
+fetch(
+    {
+        query: `{
+                query: {
+                    randomQuote: {
+                        "id",
+                        "text",
+                        "author",
+                    }
+                }
+            }`
+    }
+)
+    .then(res => res.json()
+    .then(json => window.qod = json)
+    .finally(() => {
+        let q = window.qod[Math.floor(Math.random() * window.qod.length)];
+        document.getElementById('quote').innerHTML = q.quoteText + ' - ' + '<em>' + q.quoteAuthor + '</em>';
+    })
+    .catch(err => console.error('Error!' + err.message))
+);
+
+*/
